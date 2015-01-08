071177927e5871c57c396ab209a9dcd6d8fa8680
alvarogomez93 <tiamhot12@gmail.com>
2015-01-09 00:20:44 +0100

    Comunicacion en dos direcciones

--- serv.py
+++ serv.py
@@ -1,6 +1,4 @@
 import sys
-import termios
-import tty
 
 __author__ = 'alvarogomez93'
 
@@ -8,6 +6,7 @@ import ssh
 import threading
 import socket
 import os
+
 
 class bcolors:
     HEADER = '\033[95m'
@@ -19,11 +18,17 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 class Server(ssh.ServerInterface):
+
+    usuarios = []
+
     def check_auth_password(self, username, password):
-        if (username == "test") and (password == "test"):
-            return ssh.AUTH_SUCCESSFUL
+        for comprobacion in self.usuarios:
+            if comprobacion[0] == username and comprobacion[1] == password:
+                return ssh.AUTH_SUCCESSFUL
         else:
+            print "Intento de usuario "+username+ " no auterizado"
             return ssh.AUTH_FAILED
     def check_channel_request(self, kind, chanid):
         if kind == 'session':
@@ -38,32 +43,37 @@ class Server(ssh.ServerInterface):
         return True
 
     def __init__(self):
+
         self.event = threading.Event()
-
-
-
-
-
+    try:
+        usuarios = [line.split() for line in open("usuarios.txt", 'r')]
+        print "Usuarios leidos"
+    except IOError as e:
+        f = open('usuarios.txt', 'w')
+        f.write("test test")
+        usuarios = [['test','test']]
+        f.close()
 
 
 def windows_shell(chan):
-    import threading
 
     chan.send("---Escribe escribe---.\r\n")
 
     def writeall(sock):
         while True:
             data = sock.recv(256)
-            sys.stdout.write(data)
             if data == '\r' or '\n' == data:
                 chan.send("\r\n")
-                print '\r\n',
+                print ""
             chan.send(bcolors.OKGREEN+data+bcolors.ENDC)
+            sys.stdout.write(data)
 
     writer = threading.Thread(target=writeall, args=(chan,))
     writer.start()
 
-#Inicializacion, binding al puerto 3000
+
+
+# Inicializacion, binding al puerto 3000
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(('', 3000))
@@ -76,6 +86,21 @@ else:
     nuevallave.write_private_key(open("id_rsa", 'w'), password=None)
     rsallave = ssh.RSAKey(filename="id_rsa")
 
+def imprimir(conexiones):
+
+    while True:
+
+        entrada = raw_input()
+        for conexion in conexiones:
+            conexion.send(bcolors.OKBLUE+entrada+bcolors.ENDC+"\n\r")
+
+
+conexiones = []
+responder = threading.Thread(target=imprimir, args=(conexiones,))
+responder.start()
+
+
+
 
 while True:
     try:
@@ -84,7 +109,6 @@ while True:
     except Exception as e:
         sys.exit(1)
 
-
     print "Conexion establecida"
     t = ssh.Transport(client)
     servidor = Server()
@@ -92,6 +116,8 @@ while True:
     t.start_server(server=servidor)
 
     conexion = t.accept(20)
+    conexiones.append(conexion)
+
     if conexion is None:
         print "No conexion"
         sys.exit(1)
